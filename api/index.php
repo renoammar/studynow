a575e421a104a0b9a909e409a967889f5eeaeb29
Maureno Ammar <maurenoammar4@gmail.com>
2024-12-11 23:00:40 +0700

    Update vercel.json and move index.php to api directory

--- api/index.php
+++ api/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
