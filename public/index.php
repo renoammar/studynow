71d9b6ef7123e971abf7ee4707380c2657defe60
Maureno Ammar <maurenoammar4@gmail.com>
2024-12-12 16:20:47 +0700

    conect ke db

--- public/index.php
+++ public/index.php
@@ -1,19 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+//IF $_SERVER['DOCUMENT_ROOT'] does not contain the public folder, add it
+if (strpos($_SERVER['DOCUMENT_ROOT'], "/public") === false) {
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+    $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT']."/public";
+    $_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT']."/index.php";
 
-define('LARAVEL_START', microtime(true));
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+    //get $_SERVER['REQUEST_URI']; until the first ? or # character
+    $_SERVER['PATH_INFO'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $_SERVER['PHP_SELF'] = "/index.php".$_SERVER['PATH_INFO'];
 }
+$vercelURL = getenv('VERCEL_URL');
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+putenv("APP_URL=https://$vercelURL");
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Forward Vercel requests to public index.
+require __DIR__ . "/" . "../public/index.php";
